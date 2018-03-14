ca2a1af93487f078cd3a98b23322a9ce28556b3c
priscilla22 <priscillaandrew22@gmail.com>
2018-03-15 02:05:23 +0530

    HTML files formatted

--- microservices/app/src/__init__.py
+++ microservices/app/src/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 # This line adds the hasura example routes form the hasura.py file.
 # Delete these two lines, and delete the file to remove them from your project
 # from .hasura import hasura_example

2553045ae1cd9186c666f9c221bb325608088b9d
priscilla22 <priscillaandrew22@gmail.com>
2018-03-24 12:08:04 +0530

    PDF

--- server.py
+++ server.py
@@ -16,28 +16,26 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 def index():
     return render_template("upload.html")
 
-@app.route("/upload", methods=['GET','POST'])
+@app.route("/upload", methods=['POST'])
 def upload():
-	if request.method=='POST':
-	    target = os.path.join(APP_ROOT, 'file/')
-	    print(target)
+    target = os.path.join(APP_ROOT, 'file/')
+    print(target)
 
-	    if not os.path.isdir(target):
-	        os.mkdir(target)
+    if not os.path.isdir(target):
+        os.mkdir(target)
 
-	    for file in request.files.getlist("file"):
-	        print(file)
-	        filename = file.filename
-	        destination = "/".join([target, filename])
-	        print(destination)
-	        contents = file.read()
-	        value = contents.decode(encoding='UTF-8')
-	        root = ET.fromstring(value)
-	        print(root.attrib)
-	        print(value)
+    for file in request.files.getlist("file"):
+        print(file)
+        filename = file.filename
+        destination = "/".join([target, filename])
+        print(destination)
+        contents = file.read()
+        value = contents.decode(encoding='UTF-8')
+        root = ET.fromstring(value)
+        print(root.attrib)
+        print(value)
 
-	    return render_template("complete.html",data = root.attrib)
-	return render_template("complete.html",data="")
+    return render_template("complete.html",data = root.attrib)
 
 @app.route("/getfile",methods=["POST"])
 def getfile():
@@ -157,47 +155,6 @@ def getfile():
  			 </table>
 		</body>
 	</html>
-<html lang="en">
-<body>
-  <form id="input-form" action="/getfile" method="POST" enctype="multipart/form-data">
-
-  <img src="logo-gov.png" alt="logo-gov" width="72" height="72"/>
-  <p align="right"><b>Ministry of Social Justice and Empowerment</b></p>
-
-  <br><hr><h3 align="center">Aadhaar Details</h3><br>
-    <b>UID</b>&nbsp;&nbsp;&nbsp;&nbsp;"""+uid+"""
-    <br><br>
-    <b>Name</b> <input type="text" name="name">&nbsp;&nbsp;&nbsp;&nbsp;"""+name+"""
-    <br><br>
-    <b>Gender</b> <input type="text" name="gender">&nbsp;&nbsp;&nbsp;&nbsp;"""+gender+"""
-    <br><br>
-    <b>YOB</b> <input type="text" name="yob">&nbsp;&nbsp;&nbsp;&nbsp;"""+yob+"""
-    <br><br>
-    <b>CO</b> <input type="text" name="co">&nbsp;&nbsp;&nbsp;&nbsp;"""+co+"""
-    <br><br>
-    <b>LM</b> <input type="text" name="lm">&nbsp;&nbsp;&nbsp;&nbsp;"""+lm+"""
-    <br><br>
-    <b>LOC</b> <input type="text" name="loc">&nbsp;&nbsp;&nbsp;&nbsp;"""+loc+"""
-    <br><br>
-    <b>VTC</b> <input type="text" name="vtc">&nbsp;&nbsp;&nbsp;&nbsp;"""+vtc+"""
-    <br><br>
-    <b>PO</b> <input type="text" name="po">&nbsp;&nbsp;&nbsp;&nbsp;"""+po+"""
-    <br><br>
-    <b>DIST</b> <input type="text" name="dist">&nbsp;&nbsp;&nbsp;&nbsp;"""+dist+"""
-    <br><br>
-    <b>State</b> <input type="text" name="state">&nbsp;&nbsp;&nbsp;&nbsp;"""+state+"""
-    <br><br>
-    <b>PC</b> <input type="text" name="pc">&nbsp;&nbsp;&nbsp;&nbsp;"""+pc+"""
-    <br><br>
-    <b>Income</b> <input type="text" name="inc">&nbsp;&nbsp;&nbsp;&nbsp;"""+inc+"""
-    <br><br>
-    <b>Mobile</b> <input type="text" name="mobile">&nbsp;&nbsp;&nbsp;&nbsp;"""+mobile+"""
-    <br><br>     
-  </form> 
-</body>
-</html>
-		
->>>>>>> 37d332c8cf1a9057a8e4d9e88f9b129e51d07566
 		"""		
 	
 		pdf = MyFPDF()

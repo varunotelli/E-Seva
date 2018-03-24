9fba2274f6a38d6696b3e17702e302b08b665c33
priscilla22 <priscillaandrew22@gmail.com>
2018-03-24 08:12:24 +0530

    PDF file formatted

--- server.py
+++ server.py
@@ -58,29 +58,50 @@ def getfile():
 		inc=request.form["inc"]
 
 		print("uid="+uid)
-		html="""
+
+		html = """
+		<!DOCTYPE html>
+<html lang="en">
 <body>
-	<form id="input-form" action="/getfile" method="POST" enctype="multipart/form-data">UID <input type="text" name="uid">&nbsp;&nbsp;&nbsp;&nbsp;"""+uid+"""<br><br>
-	    Name <input type="text" name="name">&nbsp;&nbsp;&nbsp;&nbsp;"""+name+"""<br><br>
-	    Gender <input type="text" name="gender">&nbsp;&nbsp;&nbsp;&nbsp;"""+gender+"""<br><br>
-	    YOB <input type="text" name="yob">&nbsp;&nbsp;&nbsp;&nbsp;"""+yob+"""<br><br>
-	    CO <input type="text" name="co">&nbsp;&nbsp;&nbsp;&nbsp;"""+co+"""<br><br>
-	    LM <input type="text" name="lm">&nbsp;&nbsp;&nbsp;&nbsp;"""+loc+"""<br><br>
-	    LOC <input type="text" name="loc">&nbsp;&nbsp;&nbsp;&nbsp;"""+lm+"""<br><br>
-	    VTC <input type="text" name="vtc">&nbsp;&nbsp;&nbsp;&nbsp;"""+vtc+"""<br><br>
-	    PO <input type="text" name="po">&nbsp;&nbsp;&nbsp;&nbsp;"""+po+"""<br><br>
-	    DIST <input type="text" name="dist">&nbsp;&nbsp;&nbsp;&nbsp;"""+dist+"""<br><br>
-	    State <input type="text" name="state">&nbsp;&nbsp;&nbsp;&nbsp;"""+state+"""<br><br>
-	    PC <input type="text" name="pc">&nbsp;&nbsp;&nbsp;&nbsp;"""+pc+"""<br><br>
-	    Income <input type="text" name="inc">&nbsp;&nbsp;&nbsp;&nbsp;"""+mobile+"""<br><br>
-	    Mobile <input type="text" name="mobile">&nbsp;&nbsp;&nbsp;&nbsp;"""+inc+"""<br><br>
+  <form id="input-form" action="/getfile" method="POST" enctype="multipart/form-data">
 
-	    
-	</form>
-			
+  <img src="logo-gov.png" alt="logo-gov" width="72" height="72"/>
+  <p align="right"><b>Ministry of Social Justice and Empowerment</b></p>
+
+  <br><hr><h3 align="center">Aadhaar Details</h3><br>
+    <b>UID</b>&nbsp;&nbsp;&nbsp;&nbsp;"""+uid+"""
+    <br><br>
+    <b>Name</b> <input type="text" name="name">&nbsp;&nbsp;&nbsp;&nbsp;"""+name+"""
+    <br><br>
+    <b>Gender</b> <input type="text" name="gender">&nbsp;&nbsp;&nbsp;&nbsp;"""+gender+"""
+    <br><br>
+    <b>YOB</b> <input type="text" name="yob">&nbsp;&nbsp;&nbsp;&nbsp;"""+yob+"""
+    <br><br>
+    <b>CO</b> <input type="text" name="co">&nbsp;&nbsp;&nbsp;&nbsp;"""+co+"""
+    <br><br>
+    <b>LM</b> <input type="text" name="lm">&nbsp;&nbsp;&nbsp;&nbsp;"""+lm+"""
+    <br><br>
+    <b>LOC</b> <input type="text" name="loc">&nbsp;&nbsp;&nbsp;&nbsp;"""+loc+"""
+    <br><br>
+    <b>VTC</b> <input type="text" name="vtc">&nbsp;&nbsp;&nbsp;&nbsp;"""+vtc+"""
+    <br><br>
+    <b>PO</b> <input type="text" name="po">&nbsp;&nbsp;&nbsp;&nbsp;"""+po+"""
+    <br><br>
+    <b>DIST</b> <input type="text" name="dist">&nbsp;&nbsp;&nbsp;&nbsp;"""+dist+"""
+    <br><br>
+    <b>State</b> <input type="text" name="state">&nbsp;&nbsp;&nbsp;&nbsp;"""+state+"""
+    <br><br>
+    <b>PC</b> <input type="text" name="pc">&nbsp;&nbsp;&nbsp;&nbsp;"""+pc+"""
+    <br><br>
+    <b>Income</b> <input type="text" name="inc">&nbsp;&nbsp;&nbsp;&nbsp;"""+mobile+"""
+    <br><br>
+    <b>Mobile</b> <input type="text" name="mobile">&nbsp;&nbsp;&nbsp;&nbsp;"""+inc+"""
+    <br><br>     
+  </form> 
 </body>
-
-"""
+</html>
+		
+		"""		
 	
 		pdf = MyFPDF()
 		#First page

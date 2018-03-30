8c1fd89884bb3b388421ed8c1b7e05260f620bd3
VanshikaSridharan <chaysonu@gmail.com>
2018-03-30 16:22:53 +0530

    Replaced Aadhar with Aadhaar

--- server.py
+++ server.py
@@ -83,7 +83,7 @@ def card():
 '''@app.route("/card",methods=["GET","POST"])
 def card():
 	if request.method=="POST":
-		if request.form["id-type"]=="aadhar-card":
+		if request.form["id-type"]=="aadhaar-card":
 			return render_template("upload.html")
 		else:
 			visible=""
@@ -152,7 +152,7 @@ def getfile():
 			<center>
 			<img align="center" src="logo-gov.png" alt="logo-gov" width="72" height="72"/></center>
 			<h2 align="center"><b>GOVERNMENT OF GUJARAT</b></h2>
-			<br><hr><h2 align="center"><b>AADHAR DETAILS</b></h2><br>
+			<br><hr><h2 align="center"><b>AADHAAR DETAILS</b></h2><br>
 			<table align="center" border="0" width="70%">
 			<tr>
 					<td width="10%"></td>

fd7e390ed2a8299d280998b1e3958e8e116aff63
priscilla22 <priscillaandrew22@gmail.com>
2018-03-30 09:11:16 +0530

    Ministry changed

--- dbconnector.py
+++ dbconnector.py
@@ -1,6 +1,6 @@
 import MySQLdb
 
 def connection():
-	conn=MySQLdb.connect(host="localhost",user="root",passwd="agpk2424",db="test")
+	conn=MySQLdb.connect(host="localhost",user="root",passwd="rootsql",db="test")
 	c=conn.cursor()
 	return c,conn

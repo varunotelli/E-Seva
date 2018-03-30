5cfbe1544c5644a3f9a2dabac42955cae527109d
Sudha247 <bmwaudi247@gmail.com>
2018-03-30 15:45:27 +0530

    minor fix

--- dbconnector.py
+++ dbconnector.py
@@ -1,6 +1,6 @@
 import MySQLdb
 
 def connection():
-	conn=MySQLdb.connect(host="localhost",user="root",passwd="vasi1997@ssn",db="test")
+	conn=MySQLdb.connect(host="localhost",user="root",passwd="agpk2424",db="test")
 	c=conn.cursor()
 	return c,conn

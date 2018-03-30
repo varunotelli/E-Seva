e2a695d8101d98421fd32fa28642316e73211852
Vasanth Kumar E <evk29797@gmail.com>
2018-03-30 14:03:00 +0530

    Added constraint for phone number

--- dbconnector.py
+++ dbconnector.py
@@ -1,6 +1,6 @@
 import MySQLdb
 
 def connection():
-	conn=MySQLdb.connect(host="localhost",user="root",passwd="sonu",db="test")
+	conn=MySQLdb.connect(host="localhost",user="root",passwd="vasi1997@ssn",db="test")
 	c=conn.cursor()
 	return c,conn
